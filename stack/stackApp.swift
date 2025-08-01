3142cde5931e9914c0fcb61788c563db57d82ba0
Kabir Teria <kabirteria@gmail.com>
2025-08-02 03:06:45 +0530

    Initial Commit

---
+++ stack/stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackApp.swift
+//  stack
+//
+//  Created by Kabir Teria on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stackTests/stackTests.swift
@@ -0,0 +1,17 @@
+//
+//  stackTests.swift
+//  stackTests
+//
+//  Created by Kabir Teria on 02/08/25.
+//
+
+import Testing
+@testable import stack
+
+struct stackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
